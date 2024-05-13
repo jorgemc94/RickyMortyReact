9cfd6320acc06891ae13f1b0ca61b6addab5dd55
jorgemc94 <jmaciasc1994@gmail.com>
2024-05-13 23:27:46 +0200

    store redux

--- src/components/FormCharcarterComponent/FormCharacterComponent.jsx
+++ src/components/FormCharcarterComponent/FormCharacterComponent.jsx
@@ -3,16 +3,21 @@ import './FormCharacterComponent.css'
 
 export const FormCharacterComponent = () => {
 
+    const submitHandler = (event) => {
+        event.preventDefault();
+        console.log(event.target.elements.name.value);
+        console.log(event.target.elements.specie.value);
+    }
 
     return(
         <>
         {
-            <form className='FormCharacterComponent__form'>
-                <label className='FormCharacterComponent__form__label'>Name</label>
-                <input className='FormCharacterComponent__form__input' type='text' placeholder='Name'></input>
-                <label className='FormCharacterComponent__form__label'>Specie</label>
-                <input className='FormCharacterComponent__form__input' type='text' placeholder='Specie'></input>
-                <button className='FormCharacterComponent__form__button' value={SubmitEvent}>Send</button>
+            <form className='FormCharacterComponent__form' onSubmit={submitHandler}>
+                <label htmlFor='name' className='FormCharacterComponent__form__label'>Name</label>
+                <input className='FormCharacterComponent__form__input' id='name' name='name' type='text' placeholder='Name'></input>
+                <label htmlFor='specie' className='FormCharacterComponent__form__label'>Specie</label>
+                <input className='FormCharacterComponent__form__input' id='specie' name='specie' type='text' placeholder='Specie'></input>
+                <button className='FormCharacterComponent__form__button' type='submit'>Send</button>
             </form>
         }
         </>
--- src/main.jsx
+++ src/main.jsx
@@ -1,24 +1,28 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Routes, Route, Navigate} from 'react-router-dom'
-import { IndexPages } from './pages/IndexPage.jsx'
-import { EpisodesPages } from './pages/EpisodesPage.jsx'
-import { NavbarComponent } from './components/NavbarComponent/NavbarComponent.jsx'
-import { EpisodeDetailsPage } from './pages/EpisodeDetailsPages/EpisodeDetailsPage.jsx'
-import { FormCharacterPage } from './pages/FormCharacterPage.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route, Navigate} from 'react-router-dom';
+import { IndexPages } from './pages/IndexPage.jsx';
+import { EpisodesPages } from './pages/EpisodesPage.jsx';
+import { NavbarComponent } from './components/NavbarComponent/NavbarComponent.jsx';
+import { EpisodeDetailsPage } from './pages/EpisodeDetailsPages/EpisodeDetailsPage.jsx';
+import { FormCharacterPage } from './pages/FormCharacterPage.jsx';
+import { Provider } from 'react-redux';
+import { store } from './app/store.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route element={<NavbarComponent />}>
-          <Route path='' element={<IndexPages/>} />
-          <Route path='/episodes' element={<EpisodesPages/>} />
-          <Route path='/episode/:id' element={<EpisodeDetailsPage/>}/>
-          <Route path='/newcharacter' element={<FormCharacterPage/>} />
-          <Route path='*' element={<Navigate to= '/' replace />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <Provider store={store} >
+      <BrowserRouter>
+        <Routes>
+          <Route element={<NavbarComponent />}>
+            <Route path='' element={<IndexPages/>} />
+            <Route path='/episodes' element={<EpisodesPages/>} />
+            <Route path='/episode/:id' element={<EpisodeDetailsPage/>}/>
+            <Route path='/newcharacter' element={<FormCharacterPage/>} />
+            <Route path='*' element={<Navigate to= '/' replace />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
 )
